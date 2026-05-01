89e9fe0d7516cf55294c1740fe09eb6ac00fa9bd
mfielder16 <fieldema98@gmail.com>
2026-05-01 17:53:41 -0400

    Set up task queue project strusture amd planning files

---
+++ task.py
@@ -0,0 +1,14 @@
+class Task:
+    def __init__(self, _id, title, description, status, priority, created_at):
+        self.id = _id
+        self.title = title
+        self.description = description
+        self.status = status
+        self.priority = priority
+        self.created_at = created_at
+
+    
+
+
+
+
