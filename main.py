9b03f487e4affc839a1f37158800a28d9c6be6af
mfielder16 <fieldema98@gmail.com>
2026-05-01 20:11:32 -0400

    Add Task class with core workflow fields

--- main.py
+++ main.py
@@ -1,1 +1,4 @@
+from task import Task
 
+task = Task(_id = 1, title="Buy groceries", description="Milk, Bread, Eggs", priority="High")
+print(task)
--- task.py
+++ task.py
@@ -1,12 +1,24 @@
+# This file defines the Task class, which represents a task in the task management system.
+# Import datetime for handling task creation time
+from datetime import date, datetime
 class Task:
-    def __init__(self, _id, title, description, status, priority, created_at):
+    # define the Task class with attributes for id, title, description, status, priority, and created_at
+    # generate timestamp for created_at if not provided
+    def __init__(self, title, description, status = "queued", priority = None, created_at = None,  _id = None):
         self.id = _id
         self.title = title
         self.description = description
         self.status = status
         self.priority = priority
-        self.created_at = created_at
+        self.created_at = datetime.now()
 
+    # method to return a string representation of the task
+    def __str__(self):
+        # return a string representation of the task with: id, title, description, status, priority, and created_at
+        return f"Task ID: {self.id}\nTitle: {self.title}\nDescription: {self.description}\nStatus: {self.status}\nPriority: {self.priority}\nCreated At: {self.created_at}"
+       
+    
+    
     
 
 
